0709a8b68facf6d759a106000e8e7f60b241438b
tinyAntelope <scott@tinyantelope.com>
2015-03-21 20:12:57 -0400

    Final Bouncy Button will work for a basic on off switch

--- buttonBuddy/BuddiesButton.swift
+++ buttonBuddy/BuddiesButton.swift
@@ -9,60 +9,67 @@
 import UIKit
 @IBDesignable
 class BuddiesButton: UIButton {
-    
-    
+    @IBInspectable
+    var defaultState: Bool = false{
+        didSet {
+            isOn = defaultState
+        }
+    }
     @IBInspectable
     var theOnColor: UIColor = UIColor.redColor(){
         didSet {
             updateLayerProperties()
         }
     }
-    
     @IBInspectable
-    var theOffColor: UIColor = UIColor.redColor(){
+    var fontOnColor: UIColor = UIColor.blackColor(){
         didSet {
             updateLayerProperties()
         }
     }
-    
+    @IBInspectable
+    var fontOffColor: UIColor = UIColor.lightGrayColor(){
+        didSet {
+            updateLayerProperties()
+        }
+    }
+    @IBInspectable
+    var theOffColor: UIColor = UIColor.darkGrayColor(){
+        didSet {
+            updateLayerProperties()
+        }
+    }
     var isOn :Bool = false{
         didSet {
             if self.isOn {
-                green()
+                buttonOn()
             }else {
-                red()
+                buttonOff()
             }
         }
     }
-    
     func updateLayerProperties(){
-            if isOn {
-                self.backgroundColor = theOnColor
-            }else   {
-                self.backgroundColor = theOffColor
-            }
-    
-    }
-    func circle(inFrame: CGRect) -> CGPath {
-        let circle = UIBezierPath(ovalInRect: inFrame)
-        return circle.CGPath
+        if isOn {
+            self.backgroundColor = theOnColor
+            self.setTitleColor(fontOnColor, forState: UIControlState.Normal)
+        }else   {
+            self.backgroundColor = theOffColor
+            self.setTitleColor(fontOffColor, forState: UIControlState.Normal)
+        }
+        
     }
     
+    func buttonOn(){
 
-    
-    func green(){
-        println("on")
-        self.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        self.setTitleColor(fontOnColor, forState: UIControlState.Normal)
         self.layoutIfNeeded()
-            UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 100, options: .CurveEaseOut, animations:{
-                self.backgroundColor = self.theOnColor
-                self.transform = CGAffineTransformMakeScale(1.0, 1.0)
+        UIView.animateWithDuration(0.75, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 100, options: .CurveEaseOut, animations:{
+            self.backgroundColor = self.theOnColor
+            self.transform = CGAffineTransformMakeScale(1.0, 1.0)
             }, completion: nil)
-
     }
-    func red(){
-        println("off")
-        self.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+    func buttonOff(){
+        self.setTitleColor(fontOffColor, forState: UIControlState.Normal)
         self.layoutIfNeeded()
         UIView.animateWithDuration(0.2, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 50, options: .CurveEaseOut, animations:{
             self.transform = CGAffineTransformMakeScale(0.9, 0.9)
@@ -73,12 +80,6 @@ class BuddiesButton: UIButton {
     override func layoutSubviews()
     {
         super.layoutSubviews()
-        if self.isOn {
-            green()
-        }else {
-            red()
-        }
         updateLayerProperties()
     }
-    
 }
